5438eb5f5456d7a5f81b662d749b2780fc0ccb0d
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2022-02-09 15:03:01 -0600

    initial commit

---
+++ recipe-idea-generator/src/recipeData.js
@@ -0,0 +1,31 @@
+export default {
+    "success": true,
+    "data": {
+        "recipes": [
+            {
+                "id": 1,
+                "name": "Palak Paneer",
+                "url": "https://i.imgflip.com/30b1gx.jpg",
+                "dietaryRestrictions": ["vegan", "glutenFree", "vegetarian"],
+                "flavorProfile": ["savory"],
+                "inspiredFrom": ["indian"]
+            },
+            {
+                "id": 2,
+                "name": "Salmon Green Curry",
+                "url": "https://minimalistbaker.com/easy-salmon-green-curry-1-pot/",
+                "dietaryRestrictions": ["glutenFree"],
+                "flavorProfile": ["savory"],
+                "inspiredFrom": ["thai"]
+            },
+            {
+                "id": 3,
+                "name": "Bibimbap with Gochujang Sauce",
+                "url": "https://minimalistbaker.com/easy-bibimbap-with-gochujang-sauce/",
+                "dietaryRestrictions": ["vegetarian"],
+                "flavorProfile": ["savory"],
+                "inspiredFrom": ["korean"]
+            },
+        ]
+    }
+}
