c539131348ad038ad2a2afed2f3da61accd61f0a
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2022-02-15 16:19:08 -0600

    initial commit

---
+++ recipe-idea-generator/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import '../navbar.css';
+
+export default function Navbar() {
+
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="">Recipe Generator</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
